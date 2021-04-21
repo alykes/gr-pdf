fa6854da156609836539e4d0ff44e847c92c779e
alykes <anthonygouskos@gmail.com>
2021-04-21 21:42:23 +1000

    messing around with different graphs

--- plot.py
+++ plot.py
@@ -2,6 +2,9 @@ import plotly
 import plotly.graph_objs as go
 import pandas as pd
 import sqlite3
+from plotly.subplots import make_subplots
+
+
 
 db_con = sqlite3.connect('gr-covid.db')
 regions = ['ΖΑΚΥΝΘΟΥ', 'ΘΕΣΣΑΛΟΝΙΚΗΣ']
@@ -9,16 +12,63 @@ regions = ['ΖΑΚΥΝΘΟΥ', 'ΘΕΣΣΑΛΟΝΙΚΗΣ']
 dataz = pd.read_sql_query("select * from summary ", db_con)
 print(dataz)
 
-subset = dataz[dataz['dt'] == "2021-01-01"]
+datez = "2021-01-01"
+subset = dataz[dataz['dt'] == datez]
 #subset = dataz[dataz['region'].isin(regions)]
-subset = dataz[dataz['region'] == "ΖΑΚΥΝΘΟΥ"]#"ΘΕΣΣΑΛΟΝΙΚΗΣ"]
+subset_zante = dataz[dataz['region'] == "ΖΑΚΥΝΘΟΥ"]#"ΘΕΣΣΑΛΟΝΙΚΗΣ"]
+subset_thess = dataz[dataz['region'] == "ΘΕΣΣΑΛΟΝΙΚΗΣ"]
 print(subset)
 
 
-plotly.offline.plot({
-    "data": [go.Bar (x = subset['dt'], y = subset['cases'])],
-        "layout": go.Layout(title="Coronavirus Cases in ΖΑΚΥΝΘΟΥ")#ΘΕΣΣΑΛΟΝΙΚΗΣ")
-})
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_bar(secondary_y=False, x=subset_zante['dt'], y = subset_zante['cases'],
+                marker_color="MediumSlateBlue", name="ΖΑΚΥΝΘΟΥ")
+fig.add_scatter(secondary_y=True,  x=subset_thess['dt'], y = subset_thess['cases'],
+                marker_color="DarkOrange", name="ΘΕΣΣΑΛΟΝΙΚΗΣ")
+fig.update_yaxes(secondary_y=False,
+                tickfont_color="MediumSlateBlue", title="Cases")
+fig.update_yaxes(secondary_y=True,
+                tickfont_color="DarkOrange", title="Cases")
+fig.update_layout(title_text="Coronavirus Cases")
+fig.show()
+
+
+plotly.io.write_html(fig, "stats.html")
+
+
+# fig = plotly.offline.plot({
+#     "data": [go.Bar (x = subset_zante['dt'], y = subset_zante['cases'])],
+#         "layout": go.Layout(title="Coronavirus Cases in ΖΑΚΥΝΘΟΥ")#ΘΕΣΣΑΛΟΝΙΚΗΣ")
+# })
+
+# from plotly.subplots import make_subplots
+# fig = make_subplots(rows=2, cols=2,
+#                     specs=[[{"type": "xy"}, {"type": "polar"}],
+#                            [{"type": "domain"}, {"type": "scene"}]])
+# fig.add_bar(row=1, col=1, y=[2, 3, 1], )
+# fig.add_pie(row=2, col=1, values=[2, 3, 1])
+# fig.add_barpolar(row=1, col=2, theta=[0, 45, 90], r=[2, 3, 1])
+# fig.add_scatter3d(row=2, col=2, x=[2, 3], y=[0, 0], z=[0.5, 1])
+# fig.update_layout(height=700, showlegend=False)
+# fig.show()
+
+#
+# fig.update_xaxes(
+#     rangeslider_visible=True,
+#     rangeselector=dict(
+#         buttons=list([
+#             dict(count=1, label="1m", step="month", stepmode="backward"),
+#             dict(count=6, label="6m", step="month", stepmode="backward"),
+#             dict(count=1, label="YTD", step="year", stepmode="todate"),
+#             dict(count=1, label="1y", step="year", stepmode="backward"),
+#             dict(step="all")
+#         ])
+#     )
+# )
+#
+# fig.show()
+
 
 # plotly.offline.plot({
 #     "data": [go.Bar(x=['Mario', 'Luigi', 'Bowser', 'Toad', 'Peach', 'Koopa'], y=[6, 5, 1, 4, 3, 2])],
