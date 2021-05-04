57465bb396db4634a74eca44ca524efa185678be
alykes <anthonygouskos@gmail.com>
2021-05-05 09:41:21 +1000

    plot rolling 7 day average

--- plot.py
+++ plot.py
@@ -24,12 +24,20 @@ print(subset)
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 fig.add_bar(secondary_y=False, x=subset_zante['dt'], y = subset_zante['cases'],
                 marker_color="MediumSlateBlue", name="ΖΑΚΥΝΘΟΥ")
-fig.add_scatter(secondary_y=True,  x=subset_thess['dt'], y = subset_thess['cases'],
-                marker_color="DarkOrange", name="ΘΕΣΣΑΛΟΝΙΚΗΣ")
+
+# fig.add_scatter(secondary_y=True,  x=subset_thess['dt'], y = subset_thess['cases'],
+#                 marker_color="DarkOrange", name="ΘΕΣΣΑΛΟΝΙΚΗΣ")
+fig.add_scatter(secondary_y=True,  x=subset_zante['dt'], y = subset_zante['avg7day'],
+                 marker_color="DarkOrange", name="Rolling 7 Day Average")
+
 fig.update_yaxes(secondary_y=False,
                 tickfont_color="MediumSlateBlue", title="Cases")
+
+# fig.update_yaxes(secondary_y=True,
+#                 tickfont_color="DarkOrange", title="Cases")
 fig.update_yaxes(secondary_y=True,
-                tickfont_color="DarkOrange", title="Cases")
+                 tickfont_color="DarkOrange", title="7 Day Average")
+
 fig.update_layout(title_text="Coronavirus Cases")
 fig.show()
 
