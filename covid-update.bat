9519abfe9c90847fae51d09e51f4889cc2c98763
alykes <anthonygouskos@gmail.com>
2022-01-07 22:57:49 +1100

    updated path to python exe

--- covid-update.bat
+++ covid-update.bat
@@ -5,9 +5,9 @@ cd e:\repos\gr-pdf
 
 del gr-covid.db
 
-python main.py
-python reader.py
-python plot.py
+e:\miniconda3\python.exe main.py
+e:\miniconda3\python.exe reader.py
+e:\miniconda3\python.exe plot.py
 
 copy stats.html .\src\index.html /Y
 git add .\src\index.html
